d4f63368a9184f37c91a8e54fd718da88e72522d
unknown <wallacec@LINUS.financialsoftware.co.uk>
2014-07-28 09:24:41 +0100

    Adding VS Project

---
+++ FSLTestAutomationExercise.Start/SearchFeature.feature
@@ -0,0 +1,25 @@
+﻿Feature: SearchFeature
+	
+
+@scenario1
+Scenario: Search by domain name
+	Given I have navigated to 'http://www.google.co.uk' in my web browser
+	And I have entered 'example.com' into the search box
+	When I click on 'Google Search'
+	Then I am presented with a list of results
+	And 'Example Domain' is the first result
+
+@scenario2
+Scenario: Search by keyword
+	Given I have navigated to 'http://www.google.co.uk' in my web browser
+	And I have entered 'example' into the search box
+	When I click on 'Google Search'
+	Then I am presented with a list of results
+	And 'Example Domain' is the fifth result
+
+@scenario3
+Scenario: I'm feeling lucky
+	Given I have navigated to 'http://www.google.co.uk' in my web browser
+	And I have entered 'example' into the search box
+	When I click on 'I'm feeling lucky'
+	Then I am automatically redirected to 'http://example.com' in my web browser
---
+++ FSLTestAutomationExercise.Start/SearchFeatureSteps.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace FSLTestAutomationExercise.Start
+{
+    [Binding]
+    public class SearchFeatureSteps
+    {
+        [Given(@"I have navigated to '(.*)' in my web browser")]
+        public void GivenIHaveNavigatedToInMyWebBrowser(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I have entered '(.*)' into the search box")]
+        public void GivenIHaveEnteredIntoTheSearchBox(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I click on '(.*)'")]
+        public void WhenIClickOn(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I click on '(.*)'m feeling lucky'")]
+        public void WhenIClickOnMFeelingLucky(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I am presented with a list of results")]
+        public void ThenIAmPresentedWithAListOfResults()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"'(.*)' is the first result")]
+        public void ThenIsTheFirstResult(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"'(.*)' is the fifth result")]
+        public void ThenIsTheFifthResult(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I am automatically redirected to '(.*)' in my web browser")]
+        public void ThenIAmAutomaticallyRedirectedToInMyWebBrowser(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
